8433b5dca3a8372cfb0b15825033074c89d518e6
Tomasz Klag <tomek.klag@gmail.com>
2019-01-16 20:49:38 +0100

    Clean.

--- src/app/youtube-search/youtube-search.component.ts
+++ src/app/youtube-search/youtube-search.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { SearchResult } from './search-result.model';
 
 @Component({
   selector: 'app-youtube-search',
   templateUrl: './youtube-search.component.html',
 })
-export class YoutubeSearchComponent implements OnInit {
+export class YoutubeSearchComponent {
   results: SearchResult[];
   loading: boolean;
 
   constructor() { }
-
-  ngOnInit() { }
 
   updateResults(results: SearchResult[]): void {
     this.results = results;
